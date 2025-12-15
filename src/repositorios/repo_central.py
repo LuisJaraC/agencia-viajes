8057412231e2de1b6f878623d5d72ee8d9a1f710
Marcos Castillo <castillo.villa3@gmail.com>
2025-12-15 20:57:18 -0300

    probando cositas

--- src/repositorios/repo_central.py
+++ src/repositorios/repo_central.py
@@ -1,6 +1,8 @@
 from .usuario_repo import UsuarioRepo
+from .rol_repo import RolRepo
 
 # DI hasta UsuarioRepo, completar el resto
 class RepoCentral():
     def __init__(self, mydb):
         self.UsuarioRepo = UsuarioRepo(mydb)
+        self.RolRepo = RolRepo(mydb)
---
+++ src/repositorios/rol_repo.py
@@ -0,0 +1,13 @@
+class RolRepo():
+    def __init__(self, mydb):
+        self.mydb = mydb
+
+    def leer_rol(self):
+        cursor = self.mydb.cursor()
+
+        cursor.execute("SELECT * FROM rol")
+        resultados = cursor.fetchall()
+
+        for rol in resultados:
+            print(rol)
+        cursor.close()
