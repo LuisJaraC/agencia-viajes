39de2433bdde75f5e522d68382998318fbd711bc
Marcos Castillo <castillo.villa3@gmail.com>
2025-12-15 23:22:31 -0300

    rol repo listo

--- src/repositorios/rol_repo.py
+++ src/repositorios/rol_repo.py
@@ -1,3 +1,5 @@
+from src.modelos.rol_modelo import Rol
+
 class RolRepo():
     def __init__(self, mydb):
         self.mydb = mydb
@@ -5,10 +7,16 @@ class RolRepo():
     def leer_rol(self):
         cursor = self.mydb.cursor()
 
-        cursor.execute("SELECT * FROM rol")
+        cursor.execute("SELECT id, rol FROM rol")
         resultados = cursor.fetchall()
 
-        for rol in resultados:
-            print(rol)
-        cursor.close()
+        # Convertimos tupla en una lista de objetos
 
+        lista_objetos_rol = []
+        for tupla in resultados:
+            rol_obj = Rol(tupla[0], tupla[1])
+            lista_objetos_rol.append(rol_obj)
+            
+        cursor.close()
+        
+        return lista_objetos_rol
