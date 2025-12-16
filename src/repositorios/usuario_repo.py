a405101618d79f98f708e089c4ededaf13518279
Luis Jara <luis.gaston.jara.coliman@gmail.com>
2025-12-16 11:36:51 -0300

    soft delete hecho en usuario

--- src/repositorios/usuario_repo.py
+++ src/repositorios/usuario_repo.py
@@ -49,6 +49,25 @@ class UsuarioRepo():
         self.mydb.commit()
         cursor.close()
 
-    # def eliminar_usuario (self):
-        # pass
+    def estado_usuario (self, id_user):
+        cursor = self.mydb.cursor()
+        # se utiliza un "NOT is_active" como inversor logico, para setear el valor opuesto del bool
+        sql = "UPDATE destino SET is_active = NOT is_active WHERE id_destino = %s"
+        val = (id_user,)
 
+        try:
+            cursor.execute(sql, val)
+            self.mydb.commit()
+            #se utiliza cursor.rowcount para validar si se hicieron cambios 
+            # > 0 si hubo cambio, == 0 no se encontró usuario o no hubieron cambios
+            if cursor.rowcount > 0:
+                print(f"Estado del usuario {id_user} cambiado exitosamente")
+                return True
+            else:
+                print("No se encontró el usuario")
+                return False
+        except mysql.connector.Error as e:
+            print(f"Error al cambiar estado: {e}")
+            return False
+        finally:
+            cursor.close()
